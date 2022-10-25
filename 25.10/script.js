59202bd0a1a80b51ba2aaefcaf896c460b06fed5
Altynai <altynaij1998@gmail.com>
2022-10-25 10:51:39 +0600

    init: homework 25.10

---
+++ 25.10/script.js
@@ -0,0 +1,14 @@
+let a = 17;
+let b = 10;
+let c = a - b;
+console.log(c);
+
+let hour = 10;
+let minute = 40;
+let seconds = 25;
+console.log(`${hour}:${minute}:${seconds}`);
+
+let num = 10;
+let result = num++;
+let number = num++;
+alert(num);
