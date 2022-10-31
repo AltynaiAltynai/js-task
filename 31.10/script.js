7aab4112828717d097b18aa7c2c1284c85752556
Altynai <altynaij1998@gmail.com>
2022-10-31 10:53:22 +0600

    init: homework 31.10

---
+++ 31.10/script.js
@@ -0,0 +1,20 @@
+let userSalaries = {
+    Ella: 100,
+    Sophia: 200,
+    Ellie: 300
+};
+console.log(userSalaries.Ella);
+console.log(userSalaries.Sophia);
+
+
+let count = {
+    a: 5,
+    b: 6,
+    c: 10
+};
+console.log(count.a + count.b + count.c);
+
+let data = 1;
+let number;
+number = data ? 3 : 5;
+console.log(number);
